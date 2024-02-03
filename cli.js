f4a65d4148d46a6c1afd6342b7c180e12a1d83f3
Alex Filatov <alariel007@gmail.com>
2024-02-03 16:15:36 +0600

    add cli command and rename project

---
+++ cli.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import CssCompiler from './lib/compiler.mjs'
+import Logger from './lib/logger.mjs'
+
+if(process.argv[2]){
+    switch(process.argv[2].toLowerCase()){
+        case 'init': CssCompiler.init(); break;
+        case 'build': CssCompiler.buildCSS(); break;
+        case 'watch': CssCompiler.buildCSS(true); break;
+        default: Logger.error('ERR CSSJ COMMAND', new Error(`Введенная команда - "${process.argv[2].toLowerCase()}", не найдена`)); break;
+    }
+}else{
+    Logger.error('ERR CSSJ COMMAND', new Error('Отсутствует аргумент соответствующий CSSJ команде'))
+}
