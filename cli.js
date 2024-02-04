afaa3abe35eb5058cf00664341fd59db4df27ffd
Alex Filatov <alariel007@gmail.com>
2024-02-05 01:03:28 +0600

    fix cli

--- cli.js
+++ cli.js
@@ -8,8 +8,8 @@ if(process.argv[2]){
         case 'init': CssCompiler.init(); break;
         case 'build': CssCompiler.buildCSS(); break;
         case 'watch': CssCompiler.buildCSS(true); break;
-        default: Logger.error('ERR CSSJ COMMAND', new Error(`Введенная команда - "${process.argv[2].toLowerCase()}", не найдена`)); break;
+        default: Logger.error('ERR CSLINER COMMAND', new Error(`Введенная команда - "${process.argv[2].toLowerCase()}", не найдена`)); break;
     }
 }else{
-    Logger.error('ERR CSSJ COMMAND', new Error('Отсутствует аргумент соответствующий CSSJ команде'))
+    Logger.error('ERR CSLINER COMMAND', new Error('Отсутствует аргумент соответствующий CSLINER команде'))
 }
